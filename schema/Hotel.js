511b68053d50a24369410d6e7709a107a2947137
juansoos <juliansotelo0825@gmail.com>
2018-02-25 16:13:15 -0500

    Se modifica el schema de hotel

--- schema/Hotel.js
+++ schema/Hotel.js
@@ -3,6 +3,7 @@ module.exports = `
   type Hotel {
     _id: ID!
     name: String!
+    image: String!
     price: Float!
     stars: Int!
     comments: [Comment]
@@ -15,12 +16,14 @@ module.exports = `
   # Input crear hotel
   input addHotel {
     name: String!
+    image: String!
     price: Float!
     stars: Int!
   }
   # Input editar curso
   input editHotel {
     name: String!
+    image: String!
     price: Float!
     stars: Int!
   }
